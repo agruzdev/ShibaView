22e79e2f34b414ad2de743d4dc2774c4be3e7ead
Alexey Gruzdev <alex.gruzdev@hotmail.com>
2021-07-23 20:54:56 +0300

    Fixed icon loading on windows

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,18 @@
 #include <iostream>
 #include <chrono>
 
+#ifdef _WIN32
+# include <Windows.h>
+# include <WinUser.h>
+#endif
+
 #include <QApplication>
 #include <QFileDialog>
 #include <QSettings>
 #include <QFileInfo>
 #include "Global.h"
 #include "ViewerApplication.h"
+
 
 namespace
 {
@@ -39,10 +45,67 @@ try
     QApplication app{argc, argv};
     QApplication::setOrganizationName(Global::kOrganizationName);
     QApplication::setApplicationName(Global::kApplicationName);
+
+#ifdef _MSC_VER
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t).count() / 1e3 << std::endl;
+#endif
+
+#ifdef _WIN32
+    do {
+        auto iconHandle = LoadImage(GetModuleHandle(NULL), L"SHIBA_VIEW_ICON", IMAGE_ICON, 0, 0, 0);
+        ICONINFO iconInfo{};
+        if (!GetIconInfo((HICON)iconHandle, &iconInfo)) {
+            break;
+        }
+
+        BITMAP bitmapInfo{};
+        if (!GetObject(iconInfo.hbmColor, sizeof(bitmapInfo), &bitmapInfo)) {
+            break;
+        }
+
+        BITMAP maskInfo{};
+        if (!GetObject(iconInfo.hbmMask, sizeof(maskInfo), &maskInfo)) {
+            break;
+        }
+
+        if (bitmapInfo.bmBitsPixel != 32 || maskInfo.bmBitsPixel != 1 || bitmapInfo.bmHeight != maskInfo.bmHeight || bitmapInfo.bmWidth != maskInfo.bmWidth) {
+            break;
+        }
+
+        const LONG pixelsCount = bitmapInfo.bmHeight * bitmapInfo.bmWidth;
+        const LONG colorSize   = pixelsCount * bitmapInfo.bmBitsPixel / 8;
+        const LONG maskSize    = pixelsCount * maskInfo.bmBitsPixel / 8;
+
+        auto color = std::make_unique<uint8_t[]>(colorSize);
+        if (colorSize != GetBitmapBits(iconInfo.hbmColor, colorSize, color.get())) {
+            break;
+        }
+
+        auto mask = std::make_unique<uint8_t[]>(maskSize);
+        if (maskSize != GetBitmapBits(iconInfo.hbmMask, maskSize, mask.get())) {
+            break;
+        }
+
+        QImage iconView = QImage(color.get(), bitmapInfo.bmWidth, bitmapInfo.bmHeight, bitmapInfo.bmWidthBytes, QImage::Format::Format_RGBA8888);
+        QImage maskView = QImage(mask.get(),  maskInfo.bmWidth,   maskInfo.bmHeight,   maskInfo.bmWidthBytes,   QImage::Format::Format_MonoLSB);
+
+        QPixmap iconPixmap = QPixmap::fromImage(iconView.rgbSwapped());
+        iconPixmap.setMask(QBitmap::fromImage(maskView.convertedTo(QImage::Format::Format_Alpha8)));
+
+        QApplication::setWindowIcon(QIcon(iconPixmap));
+
+    } while(false);
+#else // _WIN32
     QApplication::setWindowIcon(QIcon(":APPICON"));
+#endif // _WIN32
+
+#ifdef _MSC_VER
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t).count() / 1e3 << std::endl;
+#endif
 
     QString input;
     if (argc > 1) {
+#ifdef _WIN32
         input = QApplication::arguments().at(1);
         if (input == "/register") {
             return std::system("regsvr32.exe ./ShibaThumbnail.dll");
@@ -50,6 +113,10 @@ try
         else if (input == "/unregister") {
             return std::system("regsvr32.exe /u ./ShibaThumbnail.dll");
         }
+#else // _WIN32
+        std::cout << "Thumbnail service is available only on Windows" << std::endl;
+        std::exit(1);
+#endif // _WIN32
     }
     else {
         QSettings settings;
